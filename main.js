965c27da461cf780118507218430a9e4f0cbbf1f
Rajesh E <rraj241587@gmail.com>
2025-01-21 14:33:57 +0530

    modify main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.json({msg:"Hello World!"});
+});
 
 app.listen(port, () => {
-  console.log(`hi rajesh the app listening on port ${port}`)
+  console.log(`hi rajesh the app is running at http://localhost:${port}`);
 })
